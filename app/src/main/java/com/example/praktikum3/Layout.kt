24bbe520b73f709b7649f83e96ad5ca779910bfd
annisadiann <annisadian0412@gmail.com>
2025-10-23 16:16:21 +0700

    memberikan warna latar belakang abu-abu tua

--- app/src/main/java/com/example/praktikum3/Layout.kt
+++ app/src/main/java/com/example/praktikum3/Layout.kt
@@ -38,3 +38,7 @@ fun ActivitasPertama(modifier: Modifier = Modifier) {
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(all = 12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )
+        ) {
