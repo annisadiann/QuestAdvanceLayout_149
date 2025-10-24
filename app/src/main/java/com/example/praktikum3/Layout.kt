55f00962a7bc8d812fba35ac41147e2aaab14981
annisadiann <annisadian0412@gmail.com>
2025-10-24 11:53:46 +0700

    Memberi warna latar belakang biru pada kartu

--- app/src/main/java/com/example/praktikum3/Layout.kt
+++ app/src/main/java/com/example/praktikum3/Layout.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.SearchBarDefaults.colors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -80,9 +81,12 @@ fun ActivitasPertama(modifier: Modifier = Modifier) {
             }
         }
         Card
-        (modifier = Modifier
-            .fillMaxWidth(1f)
-            .padding(horizontal = 12.dp, vertical = 8.dp),
+            (modifier = Modifier
+                .fillMaxWidth(1f)
+                .padding(horizontal = 12.dp, vertical = 8.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.Blue)
+        ){
         Box(
             modifier = Modifier
                 .fillMaxSize()
